a6fb70cd167e0c48f12e3d48b805ba691431980c
DavysonMelo <mdavyson@gmail.com>
2021-10-02 15:38:18 -0300

    Added repository list to home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import NavBar from '../../components/NavBar';
+import RepositoryList from '../../components/RepositoryList';
 import { Body, Container } from './style';
 
 const Home: React.FC = () => {
   return (
     <Container>
       <NavBar />
-      <Body />
+      <Body>
+        <RepositoryList />
+      </Body>
     </Container>
   );
 };
