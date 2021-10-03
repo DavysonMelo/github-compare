a3e1511d225aeefc55514082825d7ff10dcfe372
DavysonMelo <mdavyson@gmail.com>
2021-10-03 10:13:43 -0300

    Added context provider to app file

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Home from './pages/Home';
 import '@clayui/css/lib/css/atlas.css';
 import GlobalStyle from './styles/globalStyle';
+import { ListContextProvider } from './contexts/listViewContext';
 
 function App() {
   return (
-    <>
+    <ListContextProvider>
       <Home />
       <GlobalStyle />
-    </>
+    </ListContextProvider>
   );
 }
 
