fb0f9e51d501b1d127cc2722116914a3d5d5435f
DavysonMelo <mdavyson@gmail.com>
2021-10-03 18:16:34 -0300

    Mapping props to list items

--- src/components/RepositoryList/index.tsx
+++ src/components/RepositoryList/index.tsx
@@ -1,23 +1,34 @@
 import React from 'react';
 import { useListContext } from '../../contexts/listViewContext';
+import EmptyRepoList from '../EmptyRepoList';
 import GridCard from '../GridCard';
 import RepoListItem from '../RepoListItem';
 import { GridContainer, ListContainer } from './style';
 
 const RepositoryList: React.FC = () => {
-  const { listType } = useListContext();
+  const {
+    listType,
+    filterAndSort: { repositories },
+  } = useListContext();
+
   const renderGrid = () => {
     return (
       <GridContainer>
-        <GridCard />
-        <GridCard />
-        <GridCard />
-        <GridCard />
-        <GridCard />
-        <GridCard />
-        <GridCard />
-        <GridCard />
-        <GridCard />
+        {repositories.map(repo => (
+          <GridCard
+            id={repo.id}
+            key={repo.id}
+            name={repo.full_name}
+            stars={repo.stargazers_count}
+            age={repo.created_at}
+            forks={repo.forks}
+            language={repo.language}
+            lastCommit={repo.pushed_at}
+            license={repo.license?.name}
+            openIssues={repo.open_issues}
+            starred={repo.starred || false}
+          />
+        ))}
       </GridContainer>
     );
   };
@@ -25,17 +36,28 @@ const RepositoryList: React.FC = () => {
   const renderList = () => {
     return (
       <ListContainer>
-        <RepoListItem />
-        <RepoListItem />
-        <RepoListItem />
-        <RepoListItem />
-        <RepoListItem />
-        <RepoListItem />
-        <RepoListItem />
-        <RepoListItem />
+        {repositories.map(repo => (
+          <RepoListItem
+            id={repo.id}
+            key={repo.id}
+            name={repo.full_name}
+            stars={repo.stargazers_count}
+            age={repo.created_at}
+            forks={repo.forks}
+            language={repo.language}
+            lastCommit={repo.pushed_at}
+            license={repo.license?.name}
+            openIssues={repo.open_issues}
+            starred={repo.starred || false}
+          />
+        ))}
       </ListContainer>
     );
   };
+
+  if (repositories.length === 0) {
+    return <EmptyRepoList />;
+  }
 
   return listType === 'grid' ? renderGrid() : renderList();
 };
